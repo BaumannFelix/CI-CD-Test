5c2058a72bb4806634200854b7d1e3eca5e6a53f
Felix Baumann <felix.baumann@htlwy.at>
2025-01-13 08:56:13 +0100

    Added Code

---
+++ CIDC_Test/CIDC_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDC_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welt");
+            Console.WriteLine("Das ist die Version 1.0");
+
+            Console.ReadKey();
+        }
+    }
+}
